9fac4cb9b3a80c962b1356c2da64f9158df78997
sachin pandey <91916739+sachin12031999@users.noreply.github.com>
2023-04-26 14:13:22 +0530

    Add files via upload

---
+++ build/precache-manifest.5597a3fbdd5190791c8e4969591d59b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d87b6231c79a08d1f839",
+    "url": "/static/css/main.2d324136.chunk.css"
+  },
+  {
+    "revision": "d87b6231c79a08d1f839",
+    "url": "/static/js/main.d87b6231.chunk.js"
+  },
+  {
+    "revision": "46356c4ccf608712af12",
+    "url": "/static/js/1.46356c4c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba0409fa2a2fb4cb09e9b821708127b8",
+    "url": "/index.html"
+  }
+];
